ede1f48c03941e7665111032b9ecde5c71b2787c
A7Aima <aimaabhilash@gmail.com>
2022-08-01 12:30:56 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cmp_databinding_assignment';
+  @Output() refNumber = new EventEmitter<{ incrementvalue: number }>();
+  myInterval: any;
+  incValue: number = 0;
+
+  startIncrementing(): void {
+    this.myInterval = setInterval(function () { this.incValue = this.incValue + 1; }, 1000);
+    this.refNumber.emit({
+      incrementvalue: this.incValue,
+    });
+  }
+
+  stopIncrementing(): void {
+    clearInterval(this.myInterval);
+    this.incValue = 0;
+    this.refNumber.emit({
+      incrementvalue: this.incValue,
+    });
+  }
 }
