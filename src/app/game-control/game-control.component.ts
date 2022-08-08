aa3ecfafd7f6cc73bb1b1cdb450b280cd4af434c
A7Aima <aimaabhilash@gmail.com>
2022-08-08 12:02:09 +0530

    button fix

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -7,10 +7,12 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class GameControlComponent implements OnInit {
   @Output() intervalValue = new EventEmitter<number>();
-  myInterval: any;
+  myInterval: any = null;
   incValue: number;
+  lastValue: number;
   constructor() {
     this.incValue = 0;
+    this.lastValue = 0;
   }
 
   ngOnInit(): void {
@@ -18,18 +20,31 @@ export class GameControlComponent implements OnInit {
   }
 
   startGame(): void {
-    this.myInterval = setInterval(() => {
-      this.incValue = this.incValue + 1;
-      console.log(this.incValue);
-      this.intervalValue.emit(this.incValue);
-    }, 1000);
+    if (this.myInterval === null) {
+      this.incValue = this.lastValue;
+      this.myInterval = setInterval(() => {
+        this.incValue = this.incValue + 1;
+        // console.log(this.incValue);
+        this.intervalValue.emit(this.incValue);
+      }, 1000);
+    }
 
   }
 
   stopGame(): void {
     clearInterval(this.myInterval);
+    this.myInterval = null
     this.incValue = 0;
-    console.log(this.incValue);
+    this.lastValue = 0;
+    // console.log(this.incValue);
     this.intervalValue.emit(this.incValue);
+  }
+  pauseGame(): void {
+    clearInterval(this.myInterval);
+    this.myInterval = null
+    this.lastValue = this.incValue;
+    this.incValue = -1;
+    // console.log(this.incValue);
+    // this.intervalValue.emit(this.incValue);
   }
 }
